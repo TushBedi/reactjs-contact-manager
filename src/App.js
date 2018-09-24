08e5775a364a889aa8b8a6487d3f738bacd099d8
daniel-id <daniels@outlook.co.id>
2018-09-24 22:23:37 +0700

    Creating a Form with State

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import ContactGroup from "./components/ContactGroup";
-import Header from "./components/Header";
+import ContactGroup from "./components/contacts/ContactGroup";
+import Header from "./components/layout/Header";
+import AddContact from "./components/contacts/AddContact";
 import { Provider } from "./Context";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -12,6 +13,7 @@ class App extends Component {
         <div className="App">
           <Header brand="Contact Manager" />
           <div className="container">
+            <AddContact />
             <ContactGroup />
           </div>
         </div>
