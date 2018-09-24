df33a369bf844402e561df7c21bfd868df554068
daniel-id <daniels@outlook.co.id>
2018-09-24 15:47:59 +0700

    Adding A Context Reducer For Actions

--- src/Context.js
+++ src/Context.js
@@ -2,6 +2,20 @@ import React, { Component } from "react";
 
 const Context = React.createContext();
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "DELETE_CONTACT":
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
+      };
+    default:
+      return state;
+  }
+};
+
 export class Provider extends Component {
   state = {
     contacts: [
@@ -29,7 +43,8 @@ export class Provider extends Component {
         email: "dlegit@gmail.com",
         phone: "081-222-3333"
       }
-    ]
+    ],
+    dispatch: action => this.setState(state => reducer(state, action))
   };
 
   render() {
