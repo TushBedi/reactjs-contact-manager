9e78e411f0bf63100edbf9bed31ede99ccc3d937
daniel-id <daniels@outlook.co.id>
2018-09-24 12:23:22 +0700

    Changing State From Another Component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,6 +6,11 @@ class Contact extends Component {
     showContactInfo: false
   };
 
+  onDeleteClick = () => {
+    // console.log("clicked");
+    this.props.deleteClickHandler();
+  };
+
   render() {
     const { name, email, phone } = this.props.contact;
     const { showContactInfo } = this.state;
@@ -23,6 +28,15 @@ class Contact extends Component {
             className="fas fa-sort-down"
             style={{ cursor: "pointer" }}
           />
+          <i
+            className="fas fa-times"
+            style={{
+              cursor: "pointer",
+              float: "right",
+              color: "red"
+            }}
+            onClick={this.onDeleteClick}
+          />
         </h4>
         {showContactInfo ? (
           <ul className="list-group">
@@ -36,7 +50,8 @@ class Contact extends Component {
 }
 
 Contact.propTypes = {
-  contact: PropTypes.object.isRequired
+  contact: PropTypes.object.isRequired,
+  deleteClickHandler: PropTypes.func.isRequired
 };
 
 export default Contact;
--- src/components/ContactGroup.js
+++ src/components/ContactGroup.js
@@ -64,12 +64,27 @@ class ContactGroup extends Component {
     ]
   };
 
+  deleteContact = id => {
+    // console.log(id);
+    const { contacts } = this.state;
+
+    const newContacts = contacts.filter(contact => contact.id !== id);
+
+    this.setState({
+      contacts: newContacts
+    });
+  };
+
   render() {
     const { contacts } = this.state;
     return (
       <React.Fragment>
         {contacts.map(contact => (
-          <Contact key={contact.id} contact={contact} />
+          <Contact
+            key={contact.id}
+            contact={contact}
+            deleteClickHandler={this.deleteContact.bind(this, contact.id)}
+          />
         ))}
       </React.Fragment>
     );
